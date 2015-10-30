b1fc5a66a6ecceb43cfbf9311c07e5612d34d389
lumenrain <lumenrain@gmail.com>
2015-10-30 18:32:39 +0100

    Initial

---
+++ ArrhusWebDC/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ArrhusWebDC.Models;
+using Umbraco.Web.Mvc;
+
+namespace ArrhusWebDC.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public ActionResult ShowForm()
+        {
+            ContactModel myModel = new ContactModel();
+            return PartialView("ContactForm", myModel);
+        }
+
+        public ActionResult HandleFormPost(ContactModel model)
+        {
+            var newComment = Services.ContentService.CreateContent(model.Name + " - " + DateTime.Now, CurrentPage.Id, "ContactForm");
+            newComment.SetValue("contactSender", model.Email);
+            newComment.SetValue("contactReceiver", model.Name);
+            newComment.SetValue("contactText", model.Message);
+            Services.ContentService.SaveAndPublishWithStatus(newComment);
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
